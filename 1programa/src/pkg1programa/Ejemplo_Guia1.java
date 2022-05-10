bca04a685b286f8e6a5301daf2eecbe86e035620
sebastian.ibanez1996@gmail.comgit statusgit config --global user.name sebastian.ibanez1996@gmail.comgit statusgit config --global user.name Sebastian <sebastian.ibanez1996@gmail.com>
2022-05-10 20:06:23 -0300

    NetBeans

---
+++ 1programa/src/pkg1programa/Ejemplo_Guia1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg1programa;
+
+/**
+ *
+ * @author sebas
+ */
+public class Ejemplo_Guia1 {
+    public static void main(String[] args) {
+String nombre;
+boolean bandera;
+char letra;
+}
+}
+    
+
---
+++ 1programa/src/pkg1programa/Main.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg1programa;
+
+/**
+ *
+ * @author sebas
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic her
+        //System.out.println("Hola 1° programa en Java");  
+      int num;
+      double num2;
+      float num3;
+      char palabra;
+      String palabra2;
+      boolean logico;
+      short num4;
+      
+    }
+    
+}
